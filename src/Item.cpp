6fe44c3a7c1eb973e1fc39c9f48dc2da3a5b5580
s11319 <s11319@oop.cs.nccu.edu.tw>
2025-03-26 16:34:54 +0800

    Updated the inventory system. (Yay I now know how to push individual files)

--- src/Item.cpp
+++ src/Item.cpp
@@ -1,1 +1,29 @@
 #include <iostream>
+#include <vector>
+#include "Item.h"
+#include "Status.h"
+using namespace std;
+
+int shopID = 0;
+
+Item::Item()
+	:_item{itemID++,"UnknownItem",0,0}{}
+
+Item::Item(string name,int money,int emotion)
+	:_item{itemID++,name,money,emotion}{}
+
+Item::Item(const Item& ogItem){
+	// In progress: Copy constructor
+}
+
+Item& Item::operator = Item(const Item& ogItem){
+	// In progress: Copy assignment constructor
+}
+
+Item::~Item(){
+	// In progress: Destructor
+}
+
+ItemStruct Item::getInfo(){
+	return _item;
+}
--- src/Item.h
+++ src/Item.h
@@ -1,4 +1,36 @@
 #ifndef _INVENTORY_H_
 #define _INVENTORY_H_
 
+#include <iostream>
+#include <vector>
+#include "Status.h"
+using namespace std;
+
+struct ItenStruct{
+	int itemID;
+	string name;
+	int money;
+	int emotion;
+};
+
+class Item{
+	private:
+		Itemstruct _item;
+	public:
+		Item();
+		Item(string name,int money,int emotion);
+		Item(const Item& ogItem);
+		Item& operator = (const Item& ogItem);
+		~Item();
+
+		ItemStruct getInfo();
+		void buyItem(ItemStruct item);
+};
+
+vector<Item> inventoryVec;
+// Add default inventory items here
+
+vector<Item> shopVec;
+// Add shop items here
+
 #endif
