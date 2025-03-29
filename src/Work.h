56b739476405b7a432019d18fb10afce1a41f219
s11319 <s11319@oop.cs.nccu.edu.tw>
2025-03-29 13:47:26 +0800

    Editted some moral & money values.

--- src/Work.h
+++ src/Work.h
@@ -25,9 +25,10 @@ vector<Work> workVec;
 // Add new works here
 // workVec.push_back(_name,_moral,_money);
 workVec.push_back(); // Default: Rest
-workVec.push_back("Clerk",10,100);
+workVec.push_back("Clerk",10,120);
 workVec.push_back("Vet",25,200);
 workVec.push_back("Thief",-10,200);
-workVec.push_back("Drug dealer",-50,1500);
+workVec.push_back("Robber",-25,450);
+workVec.push_back("Drug dealer",-60,1500);
 
 #endif
